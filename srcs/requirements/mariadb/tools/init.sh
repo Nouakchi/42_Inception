1ca2eccbff077e1974cfe6cca270f7cfe8b58abd
Othmane Nouakchi <onouakch@e3r8p12.1337.ma>
2024-02-04 15:32:01 +0100

    finish mariadb image

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,24 +1,12 @@
-#!/bin/bash
+service mariadb start;
 
-# starting the mysql service
-service mysql start
+mariadb -uroot -p"$SQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mariadb -uroot -p"$SQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -uroot -p"$SQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -uroot -p"$SQL_ROOT_PASSWORD" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mariadb -uroot -p"$SQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
-# change the bind to 0.0.0.0 only accept client connections made to 0.0.0.0 (accept connection to any address)
-sed -i 's/bind-address            = 127.0.0.1/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown;
+service mariadb stop;
 
-# create the database if not exist
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-
-# create the user if not exist
-mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-
-# grant all priviliges on the created database to the user
-mysql -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-
-# this command tell the MySQL or MariaDB server to reload the grant tables and update its internal data structures with the current contents of the grant tables.
-mysql -u root -e "FLUSH PRIVILEGES;"
-
-# killing the porcess of mysqld to not restarting while waiting the wordpress to get setup
-kill `cat /var/run/mysqld/mysqld.pid`
-
-mysqld
+exec mysqld;
