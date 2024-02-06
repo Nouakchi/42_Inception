07aa4a70298bcfd587f250eff46dd1790d1ae503
Othmane Nouakchi <onouakch@e3r10p12.1337.ma>
2024-02-06 17:44:08 +0100

    finish wordpress

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,2 +1,20 @@
+#!/bin/bash
 
 pwd;
+
+sleep 10;
+
+wp config create	--allow-root \
+					--dbname=$SQL_DATABASE \
+					--dbuser=$SQL_USER \
+					--dbpass=$SQL_PASSWORD \
+					--dbhost=mariadb:3306 --path='/var/www/wordpress';
+
+wp core install		--title=Example \
+					--admin_user=$SQL_USER \
+					--admin_password=$SQL_PASSWORD \
+					--admin_email=info@example.com;
+
+wp user create bob bob@example.com --role=author;
+
+exec /usr/sbin/php-fpm7.3 -F
