5ba67afd8937de7df8833300bf09466ae2477b8c
Othmane Nouakchi <onouakch@e3r7p17.1337.ma>
2024-02-07 22:31:25 +0100

    docker compose

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -4,17 +4,23 @@ pwd;
 
 sleep 10;
 
+cd /var/www/wordpress
+
 wp config create	--allow-root \
 					--dbname=$SQL_DATABASE \
 					--dbuser=$SQL_USER \
 					--dbpass=$SQL_PASSWORD \
-					--dbhost=mariadb:3306 --path='/var/www/wordpress';
+					--dbhost=mariadb:3306 \
+					--path='/var/www/wordpress'
 
-wp core install		--title=Example \
+wp core install		--allow-root \
+					--title=Example \
 					--admin_user=$SQL_USER \
 					--admin_password=$SQL_PASSWORD \
-					--admin_email=info@example.com;
+					--admin_email=info@example.com \
+					--path='/var/www/wordpress' \
+					--url='localhost'
 
-wp user create bob bob@example.com --role=author;
+wp user create bob bob@example.com --allow-root --role=author --user_pass=bob_pass
 
-exec /usr/sbin/php-fpm7.3 -F
+exec /usr/sbin/php-fpm8.2 -F
